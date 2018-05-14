a6593b35de85a4ac1303a611512849c8c4276a4d
Mona Lisa <jacobdgastelum@gmail.com>
2018-05-14 15:40:00 -0700

    fixing delete button

--- public/javascripts/create_user.js
+++ public/javascripts/create_user.js
@@ -1,36 +1,36 @@
 $(() => $("#createButton").click(createUser));
 
 function createUser() {
-    const salaryVal = $("input[name=salary]")
+  const salaryVal = $("input[name=salary]")
+    .val()
+    .trim();
+  const salary = parseInt(salaryVal, 10);
+
+  const user = {
+    name: $("input[name=name]")
       .val()
-      .trim();
-    const salary = parseInt(salaryVal, 10);
+      .trim(),
+    address: $("textarea[name=address]")
+      .val()
+      .trim(),
+    position: $("input[name=position]")
+      .val()
+      .trim(),
+    salary
+  };
 
-    const user = {
-        name: $("input[name=name]")
-          .val()
-          .trim(),
-        address: $("textarea[name=address]")
-          .val()
-          .trim(),
-        position: $("input[name=position]")
-          .val()
-          .trim(),
-        salary
-        };
+  const request = $.ajax({
+    type: "post",
+    contentType: "application/json",
+    dataType: "json",
+    data: JSON.stringify(user)
+  });
 
-    const request = $.ajax({
-        type: "post",
-        contentType: "application/json",
-        dataType: "json",
-        data: JSON.stringify(user)
-    });
+  request.done(function(data) {
+    console.log("creation done", data);
+  });
 
-    request.done(function(data) {
-        console.log("creation done", data);
-    });
-
-    request.fail(function(jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR, textStatus, errorThrown);
-    });
+  request.fail(function(jqXHR, textStatus, errorThrown) {
+    console.log(jqXHR, textStatus, errorThrown);
+  });
 }
--- public/javascripts/edit_user.js
+++ public/javascripts/edit_user.js
@@ -1,35 +1,35 @@
 $(() => $("#updateButton").click(updateUser));
 
 function updateUser() {
-    const url = window.location.pathname;
-    const userId = url.substring(url.lastIndexOf("/") + 1);
-    console.log(userId);
-    const salaryVal = $("input[name=salary]")
+  const url = window.location.pathname;
+  const userId = url.substring(url.lastIndexOf("/") + 1);
+  console.log(userId);
+  const salaryVal = $("input[name=salary]")
+    .val()
+    .trim();
+  const salary = parseInt(salaryVal, 10);
+  const user = {
+    name: $("input[name=name]")
       .val()
-      .trim();
-    const salary = parseInt(salaryVal, 10);
-    const user = {
-        name: $("input[name=name]")
-          .val()
-          .trim(),
-        address: $("textarea[name=address]")
-          .val()
-          .trim(),
-        position: $("input[name=position]")
-          .val()
-          .trim(),
-        salary
-        };
+      .trim(),
+    address: $("textarea[name=address]")
+      .val()
+      .trim(),
+    position: $("input[name=position]")
+      .val()
+      .trim(),
+    salary
+  };
 
-    const request = $.ajax({
-        type: "put",
-        contentType: "application/json",
-        dataType: "json",
-        data: JSON.stringify(user)
-    });
+  const request = $.ajax({
+    type: "put",
+    contentType: "application/json",
+    dataType: "json",
+    data: JSON.stringify(user)
+  });
 
-    request.done(data => console.log(data));
-    request.fail(function(jqXHR, textStatus, errorThrown) {
-        console.log(jqXHR, textStatus, errorThrown);
-    });
+  request.done(data => console.log(data));
+  request.fail(function(jqXHR, textStatus, errorThrown) {
+    console.log(jqXHR, textStatus, errorThrown);
+  });
 }
